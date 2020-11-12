28cf62d5cfdbc2fd2e5e44df86c907473b7a509b
ruanndias <ruan_ndias@hotmail.com>
2020-11-11 23:54:27 -0300

    adicionado swagger

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,33 +1,60 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace Api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<Data.MongoDB>();
-            services.AddControllers();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddSingleton<Data.MongoDB>();
+      services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "API COVID-19",
+          Description = "Projeto proposto por DigitalInnovationOne -> BootCamp Avanade Fullstack Developer",
+          Contact = new OpenApiContact
+          {
+            Name = "Ruan Dias",
+            Email = "ruan_ndias@hotmail.com",
+            Url = new Uri("https://twitter.com/RuanDia52736792"),
+          }
+        });
+      });
+
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API COVID-19");
+      });
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
