62cffa03f3ae9939a6aca81e43b7b405b7813c66
ruandias <ruan_ndias@hotmail.com>
2021-01-03 19:51:50 -0300

    adicionado método PUT -> AtualizarInfectado

--- Api/Controllers/InfectadoController.cs
+++ Api/Controllers/InfectadoController.cs
@@ -5,35 +5,47 @@ using MongoDB.Driver;
 
 namespace Api.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class InfectadoController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class InfectadoController : ControllerBase
+  {
+    Data.MongoDB _mongoDB;
+    IMongoCollection<Infectado> _infectadosCollection;
+
+    public InfectadoController(Data.MongoDB mongoDB)
     {
-        Data.MongoDB _mongoDB;
-        IMongoCollection<Infectado> _infectadosCollection;
-
-        public InfectadoController(Data.MongoDB mongoDB)
-        {
-            _mongoDB = mongoDB;
-            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
-        }
-
-        [HttpPost]
-        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
-        {
-            var infectado = new Infectado(dto.CodigoPaciente, dto.DataNascimento, dto.Sexo, dto.Covid19, dto.Latitude, dto.Longitude);
-
-            _infectadosCollection.InsertOne(infectado);
-            
-            return StatusCode(201, $"Infectado adicionado com sucesso {infectado.CodigoPaciente}");
-        }
-
-        [HttpGet]
-        public ActionResult ObterInfectados()
-        {
-            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
-            
-            return Ok(infectados);
-        }
+      _mongoDB = mongoDB;
+      _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
     }
+
+    [HttpPost]
+    public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+    {
+      var infectado = new Infectado(dto.CodigoPaciente, dto.DataNascimento, dto.Sexo, dto.Covid19, dto.Latitude, dto.Longitude);
+
+      _infectadosCollection.InsertOne(infectado);
+
+      return StatusCode(201, $"Infectado adicionado com sucesso {infectado.CodigoPaciente}");
+    }
+
+    [HttpGet]
+    public ActionResult ObterInfectados()
+    {
+      var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+      return Ok(infectados);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult AtualizarInfectado(string id, [FromBody] InfectadoDto dto)
+    {
+      var filter = Builders<Infectado>.Filter.Eq("codigoPaciente", id);
+
+      var update = Builders<Infectado>.Update.Set("covid19", dto.Covid19);
+
+      _infectadosCollection.UpdateOne(filter, update);
+
+      return Ok("Atualizado com sucesso!");
+    }
+  }
 }
